0eec215a5a25b87f637061d520773049f81aeafb
Emanuel Sanchez <ejsanch07@gmail.com>
2025-02-16 19:53:00 -0600

    Left some possibility for a resume timer feature later, doesn't effect script

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -3,15 +3,17 @@
         <button @click="start">Start</button>
         <button @click="stop">Stop</button>
         <button @click="reset">Reset</button>
+        <button @click="resume" v-if="paused">Resume</button>
     </div>
 </template>
 
 <script setup>
 import { defineEmits } from 'vue';
 
-const emit = defineEmits (['start', 'stop', 'reset']);
+const emit = defineEmits (['start', 'stop', 'reset', 'resume']);
 
 const start = () => emit('start');
 const stop = () => emit('stop');
 const reset = () => emit('reset');
+const resume = () => emit('resume');
 </script>
